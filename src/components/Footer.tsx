38a9db5117d65e3766bf9284b0004b1deca30e5b
MrNasaMan <davlasvegas@gmail.com>
2025-03-03 22:20:19 -0800

    minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { useQRScoutState } from '@/store/store';
 import { Heart } from 'lucide-react';
-import { Logo } from './Logo';
+import footerImage from '@/assets/images/987logo.png'; 
 
 export function Footer() {
   const teamNumber = useQRScoutState(state => state.formData.teamNumber);
@@ -8,7 +8,7 @@ export function Footer() {
     <footer>
       <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
         <div className="h-24 w-96">
-          <Logo />
+        <img src={footerImage} alt="Footer Image" className="w-96 h-48 mx-auto" />
         </div>
         {teamNumber !== 987 && (
           <>
