469e8e08df34853cf77ea96f8296d395f3661d1e
MrNasaMan <davlasvegas@gmail.com>
2025-03-08 14:29:27 -0800

    fixed #5

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',  // Output directory for build
-    // assetsDir: 'assets',  // Assets directory within dist
+    assetsDir: '',  
   },
 });
