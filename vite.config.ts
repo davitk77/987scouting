1c7f19a72ea38275b1733535fdf0f2d1c303e878
MrNasaMan <davlasvegas@gmail.com>
2025-03-08 14:38:07 -0800

    fixed #7

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/987scout/',
+  base: '',
   plugins: [react()],
   resolve: {
     alias: {
