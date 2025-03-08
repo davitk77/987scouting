817687958038a984d148132f555eb280a2c26d2a
MrNasaMan <davlasvegas@gmail.com>
2025-03-08 12:41:31 -0800

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/987scouting/',
+  base: './987scouting/',
   plugins: [react()],
   resolve: {
     alias: {
